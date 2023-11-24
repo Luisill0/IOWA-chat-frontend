e4ee2849c1e5cae65ecab7a218e517746bcfbeb7
luis naranjo <47650944+Luisill0@users.noreply.github.com>
2023-11-24 01:43:25 +0100

    remove unused code

--- src/router/conditional/index.ts
+++ src/router/conditional/index.ts
@@ -1,3 +1,2 @@
-export { default as SignedWrapper } from "./SignedWrapper";
 export { default as SocketWrapper } from "./SocketWrapper";
 export { default as UnsignedWrapper } from "./UnsignedWrapper";
